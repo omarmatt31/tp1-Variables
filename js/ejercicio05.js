f14306204749f651e7233fdf2e3edc32f98b9768
Omar Mattos <omarmatt84@gmail.com>
2025-04-12 19:22:28 -0300

    se realizó el punto 5 del tp

---
+++ js/ejercicio05.js
@@ -0,0 +1,4 @@
+const numero1 = parseInt(prompt("Ingrese el primer número")),
+numero2 = parseInt(prompt("Ingrese el segundo número"))
+
+document.writeln(numero1, " + ", numero2, " = ", numero1+numero2)
