7c9b982f3580796420891b79c081f1b5ccfcb21a
Omar Mattos <omarmatt84@gmail.com>
2025-04-15 21:51:47 -0300

    se agregaron los ejercicios de PSeInt

---
+++ js/farenheitACelsius.js
@@ -0,0 +1,2 @@
+const temperatura = Number(prompt("Ingrese la temperatura en grados Fahrenheit: "))
+document.writeln('La Temperatura en grado Celsius es: ', (5/9)*(temperatura-32))
---
+++ js/multiplicarPor10.js
@@ -0,0 +1,2 @@
+const numero = parseInt(prompt("Ingrese un número"))
+document.writeln('El resultado del número multiplicado por 10 es:'+ numero*10)
